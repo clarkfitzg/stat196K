f2680d558c14f576a8a96afafe0a8adc497248fa
Clark Fitzgerald <clarkfitzg@gmail.com>
2021-02-04 10:54:07 -0800

    got the zipped file up there

---
+++ todo.md
@@ -0,0 +1,19 @@
+
+Streaming `gzip`.
+
+```
+echo "hello there" | gzip > hello.txt.gz
+```
+
+Explain basic performance measuring tools, like `top`.
+
+Here is after I downloaded 24 GB and uploaded 3.9 GB (gzipped)
+
+```
+$ ethtool --statistics eth0
+NIC statistics:
+     rx_packets: 18870923
+     tx_packets: 4346002
+     rx_bytes: 25992601102
+     tx_bytes: 4465576805
+```
--- year_gdelt.sh
+++ year_gdelt.sh
@@ -5,5 +5,52 @@
 # Then it will take 8000/30 around 270 seconds or less than 5 minutes to stream 8 GB.
 # No problem.
 # I might as well do a full year then.
+#
+# I wonder if AWS allows streaming a compressed file?
+# Appears so, I'll try.
+#
+# M4 xlarge instances have "high" network capacity, compared to "Low to Moderate" for the t2.micro we've been using.
+# They also cost $0.20 / hour, about 15 times as much as the t2.micro.
+# Let's see how fast they are.
+#
+# Seems like it's cruising along at around 62-63 MiB/s, which is not that much better.
 
 
+# Download GDELT files for this day and upload them to S3
+DATE="2018"
+BUCKET="stat196k-data-examples"
+
+# Copy from public source into temporary directory
+mkdir ${DATE}
+time aws s3 sync s3://gdelt-open-data/v2/events/ ${DATE} --exclude "*" --include "${DATE}*"
+# real    7m12.398s
+# Copies 24 GB of data, so bandwidth is around 55 MB/s
+
+# Put them all in one object and streaming upload from standard input
+# No need for --expected-size flag, because file size is < 50 GB
+time cat ${DATE}/* |
+    gzip |
+    aws s3 cp - s3://${BUCKET}/${DATE}.csv.gz
+
+# We might get a little pipeline parallelism here.
+#
+# $ top
+#
+#   PID USER      PR  NI    VIRT    RES    SHR S  %CPU %MEM     TIME+ COMMAND
+#  3807 ec2-user  20   0    4496   1464   1180 R  98.0  0.0   0:34.00 gzip
+#  3809 ec2-user  20   0 1173572 102536  10984 S   5.0  0.6   0:02.77 aws
+#  3806 ec2-user  20   0  115832   2116    756 S   3.3  0.0   0:01.30 cat
+#
+# Nope, it appears that gzip is too much of a bottleneck, with %CPU at 98.0.
+#
+# Maybe we can try pigz for a parallel implementation
+
+# Quick sanity check before I turn this instance off and delete everything.
+
+aws s3 cp s3://${BUCKET}/${DATE}.csv.gz - | gunzip | head > first10.txt
+
+# Yep, looks good.
+
+
+# clean up
+rm -r $DATE
