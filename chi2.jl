a9dea1d1a9b498401932de1ae3be631ae87aa5b2
Clark Fitzgerald <clarkfitzg@gmail.com>
2021-02-26 10:52:03 -0800

    .

--- chi2.jl
+++ chi2.jl
@@ -1,7 +1,7 @@
-#construction
-
 using Distributions
 using StatsPlots
+
+import HypothesisTests
 
 nmax = 1e6
 
@@ -27,7 +27,7 @@ q = collect(q)
 breaks = quantile(U, q)
 
 
-# This implementation is higher level and prettier to me.
+# This implementation is higher level and more clear to me.
 function table(x, breaks)
     K = length(breaks)-1
     counts = zeros(Int, K)
@@ -58,11 +58,24 @@ end
 # Another approach
 # x_binned = CategoricalArrays.cut(x, breaks)
 
-
 x_counts = table(x, breaks)
 
-# Based on the way we constructed 
-expected_counts = 
+# All the same because we constructed them based on quantiles.
+expected_counts = fill(expected_bin_count, length(x_counts))
 
+# Pearson's Chi Squared statistic
+chi_stat = sum((x_counts - expected_counts).^2 ./ expected_counts)
 
+# Two sided P-value calculation
+chi_rv = Chisq(length(x_counts) - 1)
+pval = cdf(chi_rv, chi_stat)
+# Multiply by two to get the two sided p value, and handle the case when pval > 0.5
+pval = pval > 0.5 ? 1-pval : pval
+pval = 2 * pval
 
+# Verify statistic and pvalue calculation with external library
+expected = HypothesisTests.ChisqTest(x_counts)
+pval_check = HypothesisTests.pvalue(expected, tail = :both)
+
+# These numbers should be the same up to numerical precision, 1e-16 or so.
+abs(pval - pval_check)
